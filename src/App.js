5e55ef0dcdcef48efda0a8fbec8500fe169c66e1
Olek <20506277+olek37@users.noreply.github.com>
2019-10-06 12:57:34 +0200

    Moved data to firestore

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const {setCurrentUser} = this.props;
+    const {setCurrentUser, collectionsArray } = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
